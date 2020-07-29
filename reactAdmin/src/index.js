8da3c3716a4b795ef5797c8ed71f6b30d6c8e678
Shashank Padwal <shashank23padwal@live.com>
2020-07-30 03:35:01 +0530

    bug fixes

--- reactAdmin/src/index.js
+++ reactAdmin/src/index.js
@@ -5,7 +5,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
 import { icons } from "./assets/icons";
 
 import { Provider } from "react-redux";
